f084716d43716c0e4651f331a3a6871e1dba59d1
syedhariz1995 <syedhariz1995@gmail.com>
2023-09-18 17:02:32 +0800

    shorten the code, cleaned it

--- index.js
+++ index.js
@@ -267,78 +267,77 @@
 
 
 
-const board = document.querySelector('.board');
-const dropdown = document.getElementById('dropdown');
-const black = document.getElementById('black');
-const white = document.getElementById('white');
-const randomColour = document.getElementById('random');
-const reset = document.getElementById('reset');
+const board = document.querySelector('.board')
+const dropdown = document.getElementById('dropdown')
+const black = document.getElementById('black')
+const white = document.getElementById('white')
+const randomColour = document.getElementById('random')
+const reset = document.getElementById('reset')
 
 const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
+  const letters = '0123456789ABCDEF'
+  let color = '#'
   for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)]
   }
-  return color;
+  return color
 };
 
 const createGrid = (columns, rows) => {
-  board.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-  board.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-  board.innerHTML = '';
+  board.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
+  board.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+  board.innerHTML = ''
 
   for (let i = 0; i < columns * rows; i++) {
-    const square = document.createElement('div');
-    square.style.border = '1px solid black';
-    board.append(square);
+    const square = document.createElement('div')
+    square.style.border = '1px solid black'
+    board.append(square)
 
     black.addEventListener('click', () => {
       square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'black';
-      });
-    });
+        square.style.backgroundColor = 'black'
+      })
+    })
 
     white.addEventListener('click', () => {
       square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'white';
-      });
-    });
+        square.style.backgroundColor = 'white'
+      })
+    })
 
     randomColour.addEventListener('click', () => {
       square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = getRandomColor();
-      });
-    });
+        square.style.backgroundColor = getRandomColor()
+      })
+    })
 
     reset.addEventListener('click', () => {
-      square.style.backgroundColor = 'white';
-    });
+      square.style.backgroundColor = 'white'
+    })
   }
-};
+}
 
 const updateGrid = () => {
   const selectedValue = dropdown.value;
 
   if (selectedValue === 'option1') {
-    createGrid(1, 1);
+    createGrid(1, 1)
   } else if (selectedValue === 'option2') {
-    createGrid(2, 2);
+    createGrid(2, 2)
   } else if (selectedValue === 'option3') {
-    createGrid(4, 4);
+    createGrid(4, 4)
   } else if (selectedValue === 'option4') {
-    createGrid(8, 8);
+    createGrid(8, 8)
   } else if (selectedValue === 'option5') {
-    createGrid(16, 16);
+    createGrid(16, 16)
   } else if (selectedValue === 'option6') {
-    createGrid(32, 32);
+    createGrid(32, 32)
   } else if (selectedValue === 'option7') {
-    createGrid(64, 64);
+    createGrid(64, 64)
   } else {
-    // Default to 16x16 if an invalid option is selected
-    createGrid(16, 16);
+    createGrid(16, 16)
   }
 };
 
 dropdown.addEventListener('change', updateGrid);
-updateGrid();
+updateGrid()
