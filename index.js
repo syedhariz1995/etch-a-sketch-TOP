a974738477f01101fb3c99ba58ba3df5c9f4af8b
syedhariz1995 <syedhariz1995@gmail.com>
2023-09-18 16:13:19 +0800

    did the sketch and its functioning, but, cleaning the code to make it simpler

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
 
 const board = document.querySelector('.board')
 const dropdown = document.getElementById('dropdown')
+const black = document.getElementById('black')
+const white = document.getElementById('white')
+const randomColour = document.getElementById('random');
+const reset = document.getElementById('reset');
+
+
+const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+
 
 
 const updateGrid = () => {
     const selectedValue = dropdown.value
 
-    if(selectedValue === "option1"){
+    if (selectedValue === "option1") {
         board.style.gridTemplateColumns = 'repeat(1, 1fr)'
         board.style.gridTemplateRows = 'repeat(1, 1fr)'
         board.innerHTML = ""
-        
+
         for (let i = 0; i < 1; i++) {
             const square = document.createElement('div')
             square.style.border = '1px solid black'
             board.append(square)
+
+            black.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'black'
+                })
+            })
+
+            white.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'white'
+                })
+            })
+
+            randomColour.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                  square.style.backgroundColor = getRandomColor();
+                });
+             });
+
+             reset.addEventListener('click', () => {
+                square.style.backgroundColor = 'white'
+            })
         }
     }
 
-    if(selectedValue === "option2"){
+    if (selectedValue === "option2") {
         board.style.gridTemplateColumns = 'repeat(2, 1fr)'
         board.style.gridTemplateRows = 'repeat(2, 1fr)'
         board.innerHTML = ""
@@ -27,10 +65,32 @@ const updateGrid = () => {
             const square = document.createElement('div')
             square.style.border = '1px solid black'
             board.append(square)
+
+            black.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'black'
+                })
+            })
+
+            white.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'white'
+                })
+            })
+
+            randomColour.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                  square.style.backgroundColor = getRandomColor();
+                });
+            });
+
+            reset.addEventListener('click', () => {
+                square.style.backgroundColor = 'white'
+            })
         }
     }
 
-    if(selectedValue === "option3"){
+    if (selectedValue === "option3") {
         board.style.gridTemplateColumns = 'repeat(4, 1fr)'
         board.style.gridTemplateRows = 'repeat(4, 1fr)'
         board.innerHTML = ""
@@ -39,10 +99,32 @@ const updateGrid = () => {
             const square = document.createElement('div')
             square.style.border = '1px solid black'
             board.append(square)
+
+            black.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'black'
+                })
+            })
+
+            white.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'white'
+                })
+            })
+
+            randomColour.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                  square.style.backgroundColor = getRandomColor();
+                });
+            });
+
+            reset.addEventListener('click', () => {
+                square.style.backgroundColor = 'white'
+            })
         }
     }
 
-    if(selectedValue === "option4"){
+    if (selectedValue === "option4") {
         board.style.gridTemplateColumns = 'repeat(8, 1fr)'
         board.style.gridTemplateRows = 'repeat(8, 1fr)'
         board.innerHTML = ""
@@ -51,6 +133,28 @@ const updateGrid = () => {
             const square = document.createElement('div')
             square.style.border = '1px solid black'
             board.append(square)
+
+            black.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'black'
+                })
+            })
+
+            white.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'white'
+                })
+            })
+
+            randomColour.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                  square.style.backgroundColor = getRandomColor();
+                });
+            });
+
+            reset.addEventListener('click', () => {
+                square.style.backgroundColor = 'white'
+            })
         }
     }
 
@@ -63,6 +167,28 @@ const updateGrid = () => {
             const square = document.createElement('div')
             square.style.border = '1px solid black'
             board.append(square)
+
+            black.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'black'
+                })
+            })
+
+            white.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'white'
+                })
+            })
+
+            randomColour.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                  square.style.backgroundColor = getRandomColor();
+                });
+            });
+
+            reset.addEventListener('click', () => {
+                square.style.backgroundColor = 'white'
+            })
         }
     }
 
@@ -75,6 +201,62 @@ const updateGrid = () => {
             const square = document.createElement('div')
             square.style.border = '1px solid black'
             board.append(square)
+
+            black.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'black'
+                })
+            })
+
+            white.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'white'
+                })
+            })
+
+            randomColour.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                  square.style.backgroundColor = getRandomColor();
+                });
+            });
+
+            reset.addEventListener('click', () => {
+                square.style.backgroundColor = 'white'
+            })
+        }
+    }
+
+    if (selectedValue === 'option7') {
+        board.style.gridTemplateColumns = 'repeat(64, 1fr)'
+        board.style.gridTemplateRows = 'repeat(64, 1fr)'
+        board.innerHTML = ""
+
+        for (let i = 0; i < 4096; i++) {
+            const square = document.createElement('div')
+            square.style.border = '1px solid black'
+            board.append(square)
+
+            black.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'black'
+                })
+            })
+
+            white.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'white'
+                })
+            })
+
+            randomColour.addEventListener('click', () => {
+                square.addEventListener('mouseover', () => {
+                  square.style.backgroundColor = getRandomColor();
+                });
+            });
+
+            reset.addEventListener('click', () => {
+                square.style.backgroundColor = 'white'
+            })
         }
     }
 }
